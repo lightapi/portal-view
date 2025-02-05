74bf3e706a618921b2432001bc07afa47318c13e
Steve Hu <stevehu@gmail.com>
2025-02-05 16:57:39 -0500

    fixes #221 update PageTitle for font size

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -1,16 +1,13 @@
-import { Button } from '@mui/material';
-import React from 'react';
-// components
-import { Typography } from '../Wrappers/Wrappers';
-// styles
-import useStyles from './styles';
+import { Button } from "@mui/material";
+import { Typography } from "../Wrappers/Wrappers";
+import useStyles from "./styles";
 
 export default function PageTitle(props) {
   var classes = useStyles();
 
   return (
     <div className={classes.pageTitleContainer}>
-      <Typography className={classes.typo} variant="h1" size="sm">
+      <Typography className={classes.typo} variant="h4" size="sm">
         {props.title}
       </Typography>
       {props.button && (
