cd8d9fd6fe98b95304bd6a1d546f379da5683d75
Steve Hu <stevehu@gmail.com>
2025-05-16 23:23:01 -0400

    fixes #289 update InstanceAdmin to add more columns and button to deployment instance

--- src/pages/instance/InstanceAdmin.jsx
+++ src/pages/instance/InstanceAdmin.jsx
@@ -11,6 +11,7 @@ import TableBody from "@mui/material/TableBody";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import SystemUpdateIcon from "@mui/icons-material/SystemUpdate";
 import AddToDriveIcon from "@mui/icons-material/AddToDrive";
+import InstallDesktopIcon from "@mui/icons-material/InstallDesktop";
 import { useEffect, useState, useCallback } from "react";
 import useDebounce from "../../hooks/useDebounce.js"; // Assuming this hook exists
 import { useNavigate } from "react-router-dom";
@@ -19,6 +20,7 @@ import { useUserState } from "../../contexts/UserContext.jsx"; // Assuming this 
 import { makeStyles } from "@mui/styles";
 import PropTypes from "prop-types";
 import { apiPost } from "../../api/apiPost.js"; // Assuming this apiPost function exists
+import { stringToBoolean } from "../../utils/index.jsx";
 
 const useRowStyles = makeStyles({
   root: {
@@ -67,21 +69,33 @@ function Row(props) {
     });
   };
 
+  const handleDeploymentInstance = (instanceId) => {
+    navigate("/app/deployment/instance", {
+      state: { data: { instanceId } },
+    });
+  };
+
   return (
     <TableRow className={classes.root} key={`${row.hostId}-${row.instanceId}`}>
       <TableCell align="left">{row.hostId}</TableCell>
       <TableCell align="left">{row.instanceId}</TableCell>
       <TableCell align="left">{row.instanceName}</TableCell>
+      <TableCell align="left">{row.productVersionId}</TableCell>
       <TableCell align="left">{row.productId}</TableCell>
       <TableCell align="left">{row.productVersion}</TableCell>
       <TableCell align="left">{row.serviceId}</TableCell>
-      <TableCell align="left">{row.apiId}</TableCell>
-      <TableCell align="left">{row.apiVersion}</TableCell>
+      <TableCell align="left">{row.current ? "Yes" : "No"}</TableCell>
+      <TableCell align="left">{row.readonly ? "Yes" : "No"}</TableCell>
       <TableCell align="left">{row.environment}</TableCell>
-      <TableCell align="left">{row.pipelineId}</TableCell>
       <TableCell align="left">{row.serviceDesc}</TableCell>
       <TableCell align="left">{row.instanceDesc}</TableCell>
-      <TableCell align="left">{row.tagId}</TableCell>
+      <TableCell align="left">{row.zone}</TableCell>
+      <TableCell align="left">{row.region}</TableCell>
+      <TableCell align="left">{row.lob}</TableCell>
+      <TableCell align="left">{row.resourceName}</TableCell>
+      <TableCell align="left">{row.businessName}</TableCell>
+      <TableCell align="left">{row.envTag}</TableCell>
+      <TableCell align="left">{row.topicClassification}</TableCell>
       <TableCell align="left">{row.updateUser}</TableCell>
       <TableCell align="left">
         {row.updateTs ? new Date(row.updateTs).toLocaleString() : ""}
@@ -95,6 +109,11 @@ function Row(props) {
       <TableCell align="right">
         <AddToDriveIcon onClick={() => handleConfig(row.instanceId)} />
       </TableCell>
+      <TableCell align="right">
+        <InstallDesktopIcon
+          onClick={() => handleDeploymentInstance(row.instanceId)}
+        />
+      </TableCell>
     </TableRow>
   );
 }
@@ -105,16 +124,22 @@ Row.propTypes = {
     hostId: PropTypes.string.isRequired,
     instanceId: PropTypes.string.isRequired,
     instanceName: PropTypes.string,
+    productVersionId: PropTypes.string.isRequired,
     productId: PropTypes.string,
     productVersion: PropTypes.string,
     serviceId: PropTypes.string,
-    apiId: PropTypes.string,
-    apiVersion: PropTypes.string,
+    current: PropTypes.bool,
+    readonly: PropTypes.bool,
     environment: PropTypes.string,
-    pipelineId: PropTypes.string,
     serviceDesc: PropTypes.string,
     instanceDesc: PropTypes.string,
-    tagId: PropTypes.string,
+    zone: PropTypes.string,
+    region: PropTypes.string,
+    lob: PropTypes.string,
+    resourceName: PropTypes.string,
+    businessName: PropTypes.string,
+    envTag: PropTypes.string,
+    topicClassification: PropTypes.string,
     updateUser: PropTypes.string,
     updateTs: PropTypes.string,
   }).isRequired,
@@ -151,26 +176,38 @@ export default function InstanceAdmin() {
   const debouncedInstanceId = useDebounce(instanceId, 1000);
   const [instanceName, setInstanceName] = useState("");
   const debouncedInstanceName = useDebounce(instanceName, 1000);
+  const [productVersionId, setProductVersionId] = useState("");
+  const debouncedProductVersionId = useDebounce(productVersionId, 1000);
   const [productId, setProductId] = useState("");
   const debouncedProductId = useDebounce(productId, 1000);
   const [productVersion, setProductVersion] = useState("");
   const debouncedProductVersion = useDebounce(productVersion, 1000);
   const [serviceId, setServiceId] = useState("");
   const debouncedServiceId = useDebounce(serviceId, 1000);
-  const [apiId, setApiId] = useState("");
-  const debouncedApiId = useDebounce(apiId, 1000);
-  const [apiVersion, setApiVersion] = useState("");
-  const debouncedApiVersion = useDebounce(apiVersion, 1000);
+  const [current, setCurrent] = useState("");
+  const debouncedCurrent = useDebounce(current, 1000);
+  const [readonly, setReadonly] = useState("");
+  const debouncedReadonly = useDebounce(readonly, 1000);
   const [environment, setEnvironment] = useState("");
   const debouncedEnvironment = useDebounce(environment, 1000);
-  const [pipelineId, setPipelineId] = useState("");
-  const debouncedPipelineId = useDebounce(pipelineId, 1000);
   const [serviceDesc, setServiceDesc] = useState("");
   const debouncedServiceDesc = useDebounce(serviceDesc, 1000);
   const [instanceDesc, setInstanceDesc] = useState("");
   const debouncedInstanceDesc = useDebounce(instanceDesc, 1000);
-  const [tagId, setTagId] = useState("");
-  const debouncedTagId = useDebounce(tagId, 1000);
+  const [zone, setZone] = useState("");
+  const debouncedZone = useDebounce(zone, 1000);
+  const [region, setRegion] = useState("");
+  const debouncedRegion = useDebounce(region, 1000);
+  const [lob, setLob] = useState("");
+  const debouncedLob = useDebounce(lob, 1000);
+  const [resourceName, setResourceName] = useState("");
+  const debouncedResourceName = useDebounce(resourceName, 1000);
+  const [businessName, setBusinessName] = useState("");
+  const debouncedBusinessName = useDebounce(businessName, 1000);
+  const [envTag, setEnvTag] = useState("");
+  const debouncedEnvTag = useDebounce(envTag, 1000);
+  const [topicClassification, setTopicClassification] = useState("");
+  const debouncedTopicClassification = useDebounce(topicClassification, 1000);
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
@@ -183,6 +220,9 @@ export default function InstanceAdmin() {
   const handleInstanceNameChange = (event) => {
     setInstanceName(event.target.value);
   };
+  const handleProductVersionIdChange = (event) => {
+    setProductVersionId(event.target.value);
+  };
   const handleProductIdChange = (event) => {
     setProductId(event.target.value);
   };
@@ -192,17 +232,14 @@ export default function InstanceAdmin() {
   const handleServiceIdChange = (event) => {
     setServiceId(event.target.value);
   };
-  const handleApiIdChange = (event) => {
-    setApiId(event.target.value);
+  const handleCurrentChange = (event) => {
+    setCurrent(event.target.value);
   };
-  const handleApiVersionChange = (event) => {
-    setApiVersion(event.target.value);
+  const handleReadonlyChange = (event) => {
+    setReadonly(event.target.value);
   };
   const handleEnvironmentChange = (event) => {
     setEnvironment(event.target.value);
-  };
-  const handlePipelineIdChange = (event) => {
-    setPipelineId(event.target.value);
   };
   const handleServiceDescChange = (event) => {
     setServiceDesc(event.target.value);
@@ -210,8 +247,26 @@ export default function InstanceAdmin() {
   const handleInstanceDescChange = (event) => {
     setInstanceDesc(event.target.value);
   };
-  const handleTagIdChange = (event) => {
-    setTagId(event.target.value);
+  const handleZoneChange = (event) => {
+    setZone(event.target.value);
+  };
+  const handleRegionChange = (event) => {
+    setRegion(event.target.value);
+  };
+  const handleLobChange = (event) => {
+    setLob(event.target.value);
+  };
+  const handleResourceNameChange = (event) => {
+    setResourceName(event.target.value);
+  };
+  const handleBusinessNameChange = (event) => {
+    setBusinessName(event.target.value);
+  };
+  const handleEnvTagChange = (event) => {
+    setEnvTag(event.target.value);
+  };
+  const handleTopicClassificationChange = (event) => {
+    setTopicClassification(event.target.value);
   };
 
   const fetchData = useCallback(async (url, headers) => {
@@ -254,13 +309,22 @@ export default function InstanceAdmin() {
         productId: debouncedProductId,
         productVersion: debouncedProductVersion,
         serviceId: debouncedServiceId,
-        apiId: debouncedApiId,
-        apiVersion: debouncedApiVersion,
         environment: debouncedEnvironment,
-        pipelineId: debouncedPipelineId,
         serviceDesc: debouncedServiceDesc,
         instanceDesc: debouncedInstanceDesc,
-        tagId: debouncedTagId,
+        zone: debouncedZone,
+        region: debouncedRegion,
+        lob: debouncedLob,
+        resourceName: debouncedResourceName,
+        businessName: debouncedBusinessName,
+        envTag: debouncedEnvTag,
+        topicClassification: debouncedTopicClassification,
+        ...(debouncedCurrent && debouncedCurrent.trim() !== ""
+          ? { current: stringToBoolean(debouncedCurrent) }
+          : {}),
+        ...(debouncedReadonly && debouncedReadonly.trim() !== ""
+          ? { readonly: stringToBoolean(debouncedReadonly) }
+          : {}),
       },
     };
 
@@ -275,17 +339,23 @@ export default function InstanceAdmin() {
     host,
     debouncedInstanceId,
     debouncedInstanceName,
+    debouncedProductVersionId,
     debouncedProductId,
     debouncedProductVersion,
     debouncedServiceId,
-    debouncedApiId,
-    debouncedApiVersion,
+    debouncedCurrent,
+    debouncedReadonly,
     debouncedEnvironment,
-    debouncedPipelineId,
     debouncedServiceDesc,
     debouncedInstanceDesc,
-    debouncedTagId,
-    fetchData, // Add fetchData to dependency array of useEffect
+    debouncedZone,
+    debouncedRegion,
+    debouncedLob,
+    debouncedResourceName,
+    debouncedBusinessName,
+    debouncedEnvTag,
+    debouncedTopicClassification,
+    fetchData,
   ]);
 
   const handleChangePage = (event, newPage) => {
@@ -341,6 +411,14 @@ export default function InstanceAdmin() {
                 <TableCell align="left">
                   <input
                     type="text"
+                    placeholder="Product Version Id"
+                    value={productVersionId}
+                    onChange={handleProductVersionIdChange}
+                  />
+                </TableCell>
+                <TableCell align="left">
+                  <input
+                    type="text"
                     placeholder="Product Id"
                     value={productId}
                     onChange={handleProductIdChange}
@@ -365,17 +443,17 @@ export default function InstanceAdmin() {
                 <TableCell align="left">
                   <input
                     type="text"
-                    placeholder="Api Id"
-                    value={apiId}
-                    onChange={handleApiIdChange}
+                    placeholder="Current"
+                    value={current}
+                    onChange={handleCurrentChange}
                   />
                 </TableCell>
                 <TableCell align="left">
                   <input
                     type="text"
-                    placeholder="Api Version"
-                    value={apiVersion}
-                    onChange={handleApiVersionChange}
+                    placeholder="Readonly"
+                    value={readonly}
+                    onChange={handleReadonlyChange}
                   />
                 </TableCell>
                 <TableCell align="left">
@@ -384,14 +462,6 @@ export default function InstanceAdmin() {
                     placeholder="Environment"
                     value={environment}
                     onChange={handleEnvironmentChange}
-                  />
-                </TableCell>
-                <TableCell align="left">
-                  <input
-                    type="text"
-                    placeholder="Pipeline Id"
-                    value={pipelineId}
-                    onChange={handlePipelineIdChange}
                   />
                 </TableCell>
                 <TableCell align="left">
@@ -413,17 +483,65 @@ export default function InstanceAdmin() {
                 <TableCell align="left">
                   <input
                     type="text"
-                    placeholder="Tag Id"
-                    value={tagId}
-                    onChange={handleTagIdChange}
+                    placeholder="Zone"
+                    value={zone}
+                    onChange={handleZoneChange}
                   />
                 </TableCell>
-
+                <TableCell align="left">
+                  <input
+                    type="text"
+                    placeholder="Region"
+                    value={region}
+                    onChange={handleRegionChange}
+                  />
+                </TableCell>
+                <TableCell align="left">
+                  <input
+                    type="text"
+                    placeholder="Lob"
+                    value={lob}
+                    onChange={handleLobChange}
+                  />
+                </TableCell>
+                <TableCell align="left">
+                  <input
+                    type="text"
+                    placeholder="Resource Name"
+                    value={resourceName}
+                    onChange={handleResourceNameChange}
+                  />
+                </TableCell>
+                <TableCell align="left">
+                  <input
+                    type="text"
+                    placeholder="Business Name"
+                    value={businessName}
+                    onChange={handleBusinessNameChange}
+                  />
+                </TableCell>
+                <TableCell align="left">
+                  <input
+                    type="text"
+                    placeholder="Env Tag"
+                    value={envTag}
+                    onChange={handleEnvTagChange}
+                  />
+                </TableCell>
+                <TableCell align="left">
+                  <input
+                    type="text"
+                    placeholder="Topic Classification"
+                    value={topicClassification}
+                    onChange={handleTopicClassificationChange}
+                  />
+                </TableCell>
                 <TableCell align="left">Update User</TableCell>
                 <TableCell align="left">Update Time</TableCell>
                 <TableCell align="right">Update</TableCell>
                 <TableCell align="right">Delete</TableCell>
                 <TableCell align="right">Config</TableCell>
+                <TableCell align="right">Deployment Instance</TableCell>
               </TableRow>
             </TableHead>
             <InstanceList instances={instances} />
