b3972f6d47eba556f0c6ae1ee127244c4c5b0eb4
Steve Hu <stevehu@gmail.com>
2026-01-18 12:26:33 -0500

    fixes #418 upgrade react-schema-form and other dependencies accordingly

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CssBaseline } from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
+import { ThemeProvider as StylesThemeProvider } from '@mui/styles';
 import { theme } from './themes'
 import { LayoutProvider } from './contexts/LayoutContext.tsx'
 import { UserProvider } from './contexts/UserContext.tsx'
@@ -14,14 +15,16 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LayoutProvider>
       <ThemeProvider theme={theme}>
-        <UserProvider>
-          <SiteProvider>
-            <AppProvider>
-              <CssBaseline />
-              <App />
-            </AppProvider>
-          </SiteProvider>
-        </UserProvider>
+        <StylesThemeProvider theme={theme}>
+          <UserProvider>
+            <SiteProvider>
+              <AppProvider>
+                <CssBaseline />
+                <App />
+              </AppProvider>
+            </SiteProvider>
+          </UserProvider>
+        </StylesThemeProvider>
       </ThemeProvider>
     </LayoutProvider>
   </StrictMode>
